b5ee0b550b7189da88380550c5372b060c8b5929
santiago arteche <santiarteche@hotmail.com>
2024-03-27 04:36:13 -0300

    client.models updated

--- backend/models/clients.models.js
+++ backend/models/clients.models.js
@@ -2,18 +2,28 @@ import { Schema, model } from "mongoose";
 
 const clientSchema = new Schema(
   {
-    dni: String,
-    nombre: String,
-    apellido: String,
-    grupoFamiliar: String,
-    direccion: String,
-    telefono: String,
-    mercaderias: String,
-    prendas: String,
-    zapatillas: String,
-    microCredito: String,
-    numeroCuota: String,
-    otros: String,
+    dni: { type: String, required: true },
+    nombre: { type: String, required: true },
+    apellido: { type: String, required: true },
+    grupoFamiliar: { type: String, required: true },
+    direccion: { type: String, required: true },
+    telefono: { type: String, required: true },
+    mercaderias: {
+      type: String,
+    },
+    prendas: {
+      type: String,
+    },
+    zapatillas: {
+      type: String,
+    },
+    microCredito: {
+      type: String,
+    },
+    numeroCuota: { type: String, required: true },
+    otros: {
+      type: String,
+    },
     fecha: { type: Date, default: Date.now() },
   },
   { versionKey: false }
